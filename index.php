54c6c2d9e4181c30b4ba7d566aba581629789c74
khushbun <khushbun18@gmail.com>
2016-12-23 15:54:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,9 +21,10 @@ else {
 	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 	$user = $connection->get("account/verify_credentials");
+	
 	$statuses = $connection->get("search/tweets", ["q" =>$user->screen_name]);
 	echo $user->screen_name;
-	echo $statuses->text;
+	echo $statuses->followers_count;
 	
 	
 }
