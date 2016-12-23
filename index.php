0781bd3628bb399422ed6a976166d3a73cc53bec
khushbun <khushbun18@gmail.com>
2016-12-23 16:25:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,12 +22,5 @@ else {
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 	$user = $connection->get("account/verify_credentials");
 
-	echo $user->screen_name;
-
-	
-	
-}
-
-	
-	
+	echo $user->screen_name;	
 }
