a6556179ec991916edfaddf782bd76df67aacfd6
khushbun <khushbun18@gmail.com>
2016-12-23 14:33:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,17 +21,6 @@ else {
 	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 	$user = $connection->get("account/verify_credentials");
-	echo $user->screen_name;
+	echo "screen_name".$user->screen_name;
 	
-	$url = 'https://api.twitter.com/1.1/followers/ids.json';
-	$getfield = '?screen_name='.$user->screen_name;
-	$requestMethod = 'GET';
-
-	$twitter = new TwitterAPIExchange($settings);
-	echo $twitter->setGetfield($getfield)
-	    ->buildOauth($url, $requestMethod)
-	    ->performRequest();
-	
-	$statuses = $connection->get("search/tweets", ["q" =>$user->screen_name]);
-	echo "status".$statuses;
 }
