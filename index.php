74fb43d1aba41e44f5e3f98b3ee14e1113c82720
khushbun <khushbun18@gmail.com>
2016-12-23 17:11:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,8 +22,9 @@ else {
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 	$user = $connection->get("account/verify_credentials");
 
-	$statuses = $connection->get('statuses/'.$user->screen_name.'.json');
+	$statuses = $connection->get('1.1/statuses/'.$user->screen_name.'.json');
 	echo "status";
+	
 	
 	echo $statuses->text;
 }
