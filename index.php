8cf0a52559f3f525bf82e0d12ab443ece57f6104
khushbun <khushbun18@gmail.com>
2016-12-23 14:29:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,15 +18,15 @@ header('Location: '.$url);
 } 
 
 else {
-		$access_token = $_SESSION['access_token'];
+	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 	$user = $connection->get("account/verify_credentials");
-	$statuses = $connection->get("search/tweets", ["q" => "sweet_khushbu_n"]);
-	
-	echo 'screen nsme : '.$user->screen_name;
+	echo $user->screen_name;
+
+	$statuses = $connection->get("search/tweets", ["q" =>$user->screen_name]);
 	echo "status".$statuses;
-	
-	
+
+
 	$url = 'https://api.twitter.com/1.1/followers/ids.json';
 $getfield = '?screen_name='.$user->screen_name;
 $requestMethod = 'GET';
