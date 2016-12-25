c970b2f124e32dd08d1899b1e1272afde68456d4
khushbun <khushbun18@gmail.com>
2016-12-25 15:06:23 +0530

    Update maincontent.php

--- ui/maincontent.php
+++ ui/maincontent.php
@@ -1,27 +1,4 @@
-<?php
-echo "hiii";die();
-session_start();
-require 'autoload.php';
-use TwitterOAuth\TwitterOAuth;
- 
-define('CONSUMER_KEY', 'htjBgpI7OieRzufwsWwUA4lYU'); 
-define('CONSUMER_SECRET', 'bsZ3rejBiBexaZC004TNBSDw7XHWXWZnuFeIeV7Ckvgza2niIb'); 
-define('OAUTH_CALLBACK', 'https://rtwittertest.herokuapp.com/ui/callback.php'); 
-if (!isset($_SESSION['access_token'])) {
-	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-	$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
-	$_SESSION['oauth_token'] = $request_token['oauth_token'];
-	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
-	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
-// echo $url;
-header('Location: '.$url);
-} 
-else {
-	$access_token = $_SESSION['access_token'];
-	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
-	$user = $connection->get("account/verify_credentials");
 
-	?>
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -58,13 +35,10 @@ else {
 					
 					<div class="col-lg-3"></div>
 					<div class="col-lg-6 col-md-12 col-sm-12">
-					<?php 
-						$tweets = $connection->get('statuses/home_timeline',["count" =>10]);
-					?>
+					
 						<ul class="bxslider">
-							<?php foreach ($tweets as $result) {?>
-						  		<li><?php echo "<h2>".$result->user->name . ": " . $result->text . "</h2>"; ?></li>
-						  <?php } ?>
+						  		<li><img src="assets/images/a.jpg"/></li>
+						
 						</ul>
 						<script>
 							$(document).ready(function(){
