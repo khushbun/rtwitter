4c003ffbfb35fb7b1b85b94d8e810c4904a6c5cb
khushbun <khushbun18@gmail.com>
2016-12-25 15:22:46 +0530

    Update maincontent.php

--- ui/maincontent.php
+++ ui/maincontent.php
@@ -1,23 +1,4 @@
-<?php
-session_start();
-require 'autoload.php';
-use TwitterOAuth\TwitterOAuth;
- 
-define('CONSUMER_KEY', 'htjBgpI7OieRzufwsWwUA4lYU'); 
-define('CONSUMER_SECRET', 'bsZ3rejBiBexaZC004TNBSDw7XHWXWZnuFeIeV7Ckvgza2niIb'); 
-define('OAUTH_CALLBACK', 'https://rtwittertest.herokuapp.com/ui/callback.php'); 
-if (!isset($_SESSION['access_token'])) {
-	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-	$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
-	$_SESSION['oauth_token'] = $request_token['oauth_token'];
-	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
-	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
-// echo $url;
-header('Location: '.$url);
-	
-} 
 
-?>
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -79,7 +60,23 @@ header('Location: '.$url);
 						<ul class="list-group"style="color:black;">
 						
 
-						<?php else {
+						<?php session_start();
+require 'autoload.php';
+use TwitterOAuth\TwitterOAuth;
+ 
+define('CONSUMER_KEY', 'htjBgpI7OieRzufwsWwUA4lYU'); 
+define('CONSUMER_SECRET', 'bsZ3rejBiBexaZC004TNBSDw7XHWXWZnuFeIeV7Ckvgza2niIb'); 
+define('OAUTH_CALLBACK', 'https://rtwittertest.herokuapp.com/ui/callback.php'); 
+if (!isset($_SESSION['access_token'])) {
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+	$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
+	$_SESSION['oauth_token'] = $request_token['oauth_token'];
+	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+// echo $url;
+header('Location: '.$url);
+	
+} else {
 	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 	$user = $connection->get("account/verify_credentials");
