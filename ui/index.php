f21feffa9b478b9f29be6134f9811bcba449aafa
khushbun <khushbun18@gmail.com>
2016-12-25 14:51:37 +0530

    Add files via upload

---
+++ ui/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>twitter signup</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link href="assets/lib/jquery.bxslider.css" rel="stylesheet" />
+		
+		<script src="assets/js/jquery.bxslider.min.js"></script>
+	</head>
+	<body style="background-color: black; color:#fff;">
+		<center>
+			<div class="container" style="padding-top:5%;">
+				<div class="row">
+					<div class="col-sm-12 col-md-12 col-lg-12">
+						<span ><img src="assets/images/t.png" alt="" style="border-radius: 50%;" /></span>
+					</div>
+					<div class="col-sm-12 col-m	d-12 col-lg-12" style="padding-top:5%;">
+						<h2>RTcamp Assignment | Twitter Login</h2>
+						<p>See your 'Tweets', 'Timeline Tweets' <br/>and 'Followers' after authentication</p>
+					</div>
+					<div class="col-sm-12 col-md-12 col-lg-12" style="">
+						<a href="https://rtwittertest.herokuapp.com/maincontent.php" >
+							<button type="button" class="btn btn-primary">Sign In</button>
+						</a>
+					</div>
+				</div>		
+			</div>
+		</center>
+	</body>
+</html>
---
+++ ui/maincontent.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+require 'autoload.php';
+use TwitterOAuth\TwitterOAuth;
+ 
+define('CONSUMER_KEY', 'htjBgpI7OieRzufwsWwUA4lYU'); 
+define('CONSUMER_SECRET', 'bsZ3rejBiBexaZC004TNBSDw7XHWXWZnuFeIeV7Ckvgza2niIb'); 
+define('OAUTH_CALLBACK', 'https://rtwittertest.herokuapp.com/ui/callback.php'); 
+if (!isset($_SESSION['access_token'])) {
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+	$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
+	$_SESSION['oauth_token'] = $request_token['oauth_token'];
+	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+// echo $url;
+header('Location: '.$url);
+} 
+else {
+	$access_token = $_SESSION['access_token'];
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+	$user = $connection->get("account/verify_credentials");
+
+	?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>twitter signup</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link href="assets/lib/jquery.bxslider.css" rel="stylesheet" />
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/jquery.bxslider.min.js"></script>
+	</head>
+	<body style="background-color: black; color:#fff;">
+		<center>
+			<div class="container" style="padding-top:5%;">
+				<div class="row">
+				
+					<div class="col-lg-3"></div>
+					<div class="col-lg-6"><h1>Tweets and Followers</h1></div>
+
+				</div>
+				<div class="row" style="padding-top:5%;">
+				
+					<div class="col-lg-1"></div>
+					<div class="col-lg-10">
+						 <div class="form-group has-feedback">
+            		<label for="search" class="sr-only">Search followers</label>
+            		<input type="text" class="form-control" name="search" id="search" placeholder="search">
+              		<span class="glyphicon glyphicon-search form-control-feedback"></span>
+            	</div>
+					</div>
+
+				</div>
+				<div class="row">
+					
+					<div class="col-lg-3"></div>
+					<div class="col-lg-6 col-md-12 col-sm-12">
+					<?php 
+						$tweets = $connection->get('statuses/home_timeline',["count" =>10]);
+					?>
+						<ul class="bxslider">
+							<?php foreach ($tweets as $result) {?>
+						  		<li><?php echo "<h2>".$result->user->name . ": " . $result->text . "</h2>"; ?></li>
+						  <?php } ?>
+						</ul>
+						<script>
+							$(document).ready(function(){
+							  $('.bxslider').bxSlider();
+							});
+						</script>
+					</div>
+				</div>	
+				<div class="row">
+					<div class="col-lg-4"></div>
+					
+					<div class="col-lg-3"><h4>Followers</h4></div>
+				</div>
+				<div class="row" >
+					
+					
+					<div class="col-lg-3"></div>
+					<div class="col-lg-6 col-md-12 col-sm-12">
+						<ul class="list-group"style="color:black;">
+						    <li class="list-group-item">First item</li>
+						    <li class="list-group-item">Second item</li>
+						    <li class="list-group-item">Third item</li>
+					  	</ul>	
+		
+					</div>
+				</div>
+				 	</div>
+		</center>
+	</body>
+</html>
