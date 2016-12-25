99134bab7301ae5479e19508da7585a74b51b8ef
khushbun <khushbun18@gmail.com>
2016-12-25 15:08:51 +0530

    Update maincontent.php

--- ui/maincontent.php
+++ ui/maincontent.php
@@ -5,7 +5,17 @@ use TwitterOAuth\TwitterOAuth;
  
 define('CONSUMER_KEY', 'htjBgpI7OieRzufwsWwUA4lYU'); 
 define('CONSUMER_SECRET', 'bsZ3rejBiBexaZC004TNBSDw7XHWXWZnuFeIeV7Ckvgza2niIb'); 
-define('OAUTH_CALLBACK', 'https://rtwittertest.herokuapp.com/ui/callback.php'); ?>
+define('OAUTH_CALLBACK', 'https://rtwittertest.herokuapp.com/ui/callback.php'); 
+if (!isset($_SESSION['access_token'])) {
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+	$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
+	$_SESSION['oauth_token'] = $request_token['oauth_token'];
+	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+// echo $url;
+header('Location: '.$url);
+} 
+?>
 <!DOCTYPE HTML>
 <html>
 	<head>
