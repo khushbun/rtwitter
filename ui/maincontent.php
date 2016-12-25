64cb87f1db499a3b3f0392226d799b8a87757ce3
khushbun <khushbun18@gmail.com>
2016-12-25 15:10:16 +0530

    Update maincontent.php

--- ui/maincontent.php
+++ ui/maincontent.php
@@ -14,7 +14,14 @@ if (!isset($_SESSION['access_token'])) {
 	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
 // echo $url;
 header('Location: '.$url);
+	
 } 
+
+else {
+	$access_token = $_SESSION['access_token'];
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+	$user = $connection->get("account/verify_credentials");
+}
 ?>
 <!DOCTYPE HTML>
 <html>
