fa0c3f3a17daf8ff9e1023c44ca40361d1883554
khushbun <khushbun18@gmail.com>
2016-12-25 15:13:32 +0530

    Update maincontent.php

--- ui/maincontent.php
+++ ui/maincontent.php
@@ -21,10 +21,9 @@ else {
 	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 	$user = $connection->get("account/verify_credentials");
-	echo "TimelineTweets :<br/>";
+	
 	$tweets = $connection->get('statuses/home_timeline',["count" =>10]);
-	foreach ($tweets as $result) {
-  		echo $result->user->name . ": " . $result->text . "<br/>";}
+	}
 ?>
 <!DOCTYPE HTML>
 <html>
