0931e4153638835e4310866892c6c4a1a47ba098
khushbun <khushbun18@gmail.com>
2016-12-26 22:20:38 +0530

    Update ahtml.php

--- ui/ahtml.php
+++ ui/ahtml.php
@@ -5,9 +5,9 @@ if(isset($_POST["d"]))
   $data = $_POST['d'];
   $filename = "Tweets.json";
   file_put_contents($filename, $data);
-
+/*
   if (file_exists($filename)) {
-    /*
+    
       header('Content-Description: File Transfer');
       header('Content-Type: application/octet-stream');
       header('Content-Disposition: attachment; filename="'.basename($filename).'"');
@@ -17,15 +17,7 @@ if(isset($_POST["d"]))
       header('Content-Length: ' . filesize($filename));
       readfile($filename);
       exit;
-      */
-    //$fp = fopen('php://output', 'w');
-
-
-    header('Content-type: application/json');
-    header('Content-Disposition: attachment; filename='.$filename);
-    //fput($fp, $header);
-    readfile($filename);
-  }
+  }*/
 }
 
 
