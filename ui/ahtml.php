094554580aaee59bd7e6837212db466f7d7c6f19
khushbun <khushbun18@gmail.com>
2016-12-26 22:25:37 +0530

    Update ahtml.php

--- ui/ahtml.php
+++ ui/ahtml.php
@@ -5,11 +5,11 @@ if(isset($_POST["d"]))
   $data = $_POST['d'];
   $filename = "Tweets.json";
   file_put_contents($filename, $data);
-/*
+
   if (file_exists($filename)) {
     
       header('Content-Description: File Transfer');
-      header('Content-Type: application/octet-stream');
+      header('Content-Type: application/json');
       header('Content-Disposition: attachment; filename="'.basename($filename).'"');
       header('Expires: 0');
       header('Cache-Control: must-revalidate');
@@ -17,7 +17,7 @@ if(isset($_POST["d"]))
       header('Content-Length: ' . filesize($filename));
       readfile($filename);
       exit;
-  }*/
+  }
 }
 
 
