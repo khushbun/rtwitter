4cbe1c8aa0bd50eeb738f09ea1729ab03ab775ce
khushbun <khushbun18@gmail.com>
2016-12-26 12:04:08 +0530

    Update maincontent.php

--- ui/maincontent.php
+++ ui/maincontent.php
@@ -68,7 +68,7 @@
 				<div class="row" >
 					<div class="col-lg-1">
 						<script>
-							var data = [['<?php echo json_encode($e); ?>']];
+							var data = [['<?php echo json_encode($tweets); ?>']];
 							 var str = '';
 
 							function download_csv() {
