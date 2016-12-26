729b02d183a4c92e26f4e5ec3cb970146e47b31b
khushbun <khushbun18@gmail.com>
2016-12-26 16:32:17 +0530

    Update ahtml.php

--- ui/ahtml.php
+++ ui/ahtml.php
@@ -1,10 +1,11 @@
 <?php 
+function download_file($a){
 require('assets/lib/fpdf.php');
 $a = unserialize($_POST['input_name']);
 echo $a;
-// $pdf = new FPDF();
-// $pdf->AddPage();
-// $pdf->SetFont('Arial','B',16);
-// $pdf->Cell(40,10,$a);
-// $pdf->Output();
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,$a);
+$pdf->Output();}
 ?>
