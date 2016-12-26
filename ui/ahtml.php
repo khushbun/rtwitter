267fb293c6c5efaad039d0e74a29dee04083f1aa
khushbun <khushbun18@gmail.com>
2016-12-26 15:22:39 +0530

    Update ahtml.php

--- ui/ahtml.php
+++ ui/ahtml.php
@@ -1,12 +1,12 @@
 <?php 
 $a=$_GET['a'];
 echo "hello".$a;
-// $filename = "tweets.csv";
-// $fp = fopen('php://output', 'w');
-// $result =$a;
-// header('Content-type: application/csv');
-// header('Content-Disposition: attachment; filename='.$filename);
-// fputcsv($fp, $header);
-// $result = $a;
+$filename = "tweets.txt";
+$fp = fopen('php://output', 'w');
+$result =$a;
+header('Content-type: application/txt');
+header('Content-Disposition: attachment; filename='.$filename);
+fputcsv($fp, $header);
+$result = $a;
 
 ?>
