eacdf8edf0e272a584ef44c682d7438882ef965c
khushbun <khushbun18@gmail.com>
2016-12-26 20:34:01 +0530

    Update maincontent.php

--- ui/maincontent.php
+++ ui/maincontent.php
@@ -91,7 +91,7 @@
 
 							$(document).ready(function(){
 								$(document).on("click", "#click", function(){
-								        var s = "<?php echo $a; ?>";
+								        var s = "<?php echo json_encode($a); ?>";
 									console.log(s);
 									alert(JSON.stringify(s));
 									$.ajax({
