57c61f88d493db8560d275ad8f7b7906220a7f23
khushbun <khushbun18@gmail.com>
2016-12-26 23:25:46 +0530

    Update ahtml.php

--- ui/ahtml.php
+++ ui/ahtml.php
@@ -5,14 +5,14 @@ if(isset($_POST["d"]))
   $data = $_POST["d"];
   $filename = "Tweets.json";
   file_put_contents($filename, $data);
-
+  /*
   if (file_exists($filename)) {
     
       header('Content-Type: application/json');
       header('Content-Disposition: attachment; filename='.$filename);
       readfile($filename);
       exit;
-  }
+  }*/
 }
 
 
