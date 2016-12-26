a34e62be3695bd6e0f9c28e20551b3c57e458952
khushbun <khushbun18@gmail.com>
2016-12-26 21:07:28 +0530

    Update ahtml.php

--- ui/ahtml.php
+++ ui/ahtml.php
@@ -1,20 +1,22 @@
 <?php
 
-$data = $_POST['d'];
- 
-$filename = "Tweets.json";
-file_put_contents($filename, $data);
+if(isset($_POST["d"]))
+{
+  $data = $_POST['d'];
+  $filename = "Tweets.json";
+  file_put_contents($filename, $data);
 
-if (file_exists($filename)) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.basename($filename).'"');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($filename));
-    readfile($filename);
-    echo "true";
+  if (file_exists($filename)) {
+      header('Content-Description: File Transfer');
+      header('Content-Type: application/octet-stream');
+      header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+      header('Expires: 0');
+      header('Cache-Control: must-revalidate');
+      header('Pragma: public');
+      header('Content-Length: ' . filesize($filename));
+      readfile($filename);
+      echo "true";
+  }
 }
 
 
