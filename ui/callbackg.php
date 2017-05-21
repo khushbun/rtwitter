381dba60e60545a656f2c0afd1b6f4a83f86f4f7
Khushboo_nathwani94 <khushboo.nathwani@actonate.com>
2017-05-21 21:56:05 +0530

    added followers.php

--- ui/callbackg.php
+++ ui/callbackg.php
@@ -1,10 +1,11 @@
 <?php 
 session_start();
-require 'google-api/src/Google/autoload.php';
+require 'vendor/autoload.php';
 // 946809333321-k3v94qcjogi8upu5un8sg5534o4cst17.apps.googleusercontent.com === clientid
 
 
 // HXrV31w86cO2LGDjI-LeDfvt == clientsecret
+echo "hii";
 $fileMetadata = new Google_Service_Drive_DriveFile(array(
   'name' => 'My Report',
   'mimeType' => 'application/vnd.google-apps.spreadsheet'));
