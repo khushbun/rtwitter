eeeefac6bb17d0472280dd7f29450d51fa77bf73
Khushboo_nathwani94 <khushboo.nathwani@actonate.com>
2017-05-21 18:49:22 +0530

    added followers.php

--- ui/followers.php
+++ ui/followers.php
@@ -1,14 +1,29 @@
 <?php
 	session_start();
+
+	require 'autoload.php';
+	use TwitterOAuth\TwitterOAuth;
+	include("ahtml.php");
+
+	define('CONSUMER_KEY', 'htjBgpI7OieRzufwsWwUA4lYU'); 
+	define('CONSUMER_SECRET', 'bsZ3rejBiBexaZC004TNBSDw7XHWXWZnuFeIeV7Ckvgza2niIb'); 
+	define('OAUTH_CALLBACK', 'https://rtwittertest.herokuapp.com/ui/callback.php'); 
+
+
+	
 	echo "hii".$_SESSION['test'];
 
-	$conn = $_SESSION['connection'];
+	//$conn = $_SESSION['connection'];
 	$user = $_SESSION['user'];
 	print_r($_SESSION['access_token']);
 	print_r($user->screen_name);
 
 
 	echo "done";
+
+	$access_token = $_SESSION['access_token'];
+	$conn = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+		
 
 	$utweets = $conn->get('search/tweets',["q" =>'17_harshil']);
 	 $followers = $conn->get('followers/list',["screen_name" =>$user->screen_name, "count"=>10]);
