af95a638d985fbfec3b6c90666bc46d8d657f4a7
Khushboo_nathwani94 <khushboo.nathwani@actonate.com>
2017-05-21 18:30:36 +0530

    added followers.php

--- ui/followers.php
+++ ui/followers.php
@@ -4,11 +4,14 @@
 
 	$conn = $_SESSION['connection'];
 	$user = $_SESSION['user'];
+	print_r($conn);
+	print_r($user);
 
 
+	echo "done";
 	$utweets = $conn->get('search/tweets',["q" =>'17_harshil']);
 	// $followers = $conn->get('followers/list',["screen_name" =>$user->screen_name, "count"=>10]);
-	
+
 	// print_r($followers);	
 	print_r($utweets);	
 	foreach ($utweets->statuses as $res) {
