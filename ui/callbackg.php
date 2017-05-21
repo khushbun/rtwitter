7124f1f1009435808b98cc1bf64e779c726f2338
Khushboo_nathwani94 <khushboo.nathwani@actonate.com>
2017-05-21 21:53:03 +0530

    added followers.php

---
+++ ui/callbackg.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+require 'google-api/src/Google/autoload.php';
+// 946809333321-k3v94qcjogi8upu5un8sg5534o4cst17.apps.googleusercontent.com === clientid
+
+
+// HXrV31w86cO2LGDjI-LeDfvt == clientsecret
+$fileMetadata = new Google_Service_Drive_DriveFile(array(
+  'name' => 'My Report',
+  'mimeType' => 'application/vnd.google-apps.spreadsheet'));
+$content = file_get_contents('files/test.csv');
+$file = $driveService->files->create($fileMetadata, array(
+  'data' => $content,
+  'mimeType' => 'text/csv',
+  'uploadType' => 'multipart',
+  'fields' => 'id'));
+printf("File ID: %s\n", $file->id);
+
+?>
+
