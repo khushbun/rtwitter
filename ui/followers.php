f65cd16cb5a7dcd6dd83bd551ad0b0978c0d1dad
Khushboo_nathwani94 <khushboo.nathwani@actonate.com>
2017-05-21 18:09:46 +0530

    added followers.php

--- ui/followers.php
+++ ui/followers.php
@@ -1,4 +1,12 @@
 <?php
 	session_start();
 	echo "hii".$_SESSION['test'].$_SESSION['access_token'];
+
+	$conn = $_SESSION['connection'];
+	$utweets = $connection->get('search/tweets',["q" =>'17_harshil']);
+			
+	foreach ($utweets->statuses as $res) {
+		echo $res;
+		
+	}
 ?>
