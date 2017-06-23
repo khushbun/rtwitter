08f43ef9589c351677ef259464095b275e5a4bcd
Khushboo_nathwani94 <khushboo.nathwani@actonate.com>
2017-06-23 18:21:02 +0530

    testing

--- ui/followers.php
+++ ui/followers.php
@@ -15,7 +15,7 @@
 	$conn = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 		
 
-	$utweets = $conn->get('search/tweets.json',["q" =>"23superbowl",'count' => '10']);
+	$utweets = $conn->get('statuses/user_timeline',["screen_name" =>$_POST["user"],'count' => '10']);
 	 // https://api.twitter.com/1.1/search/tweets.json?q=%23superbowl
 		
 	$temp = [];
