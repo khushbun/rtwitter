0c95838d2864d44cfca5052607f6256396e4e245
Khushboo_nathwani94 <khushboo.nathwani@actonate.com>
2017-06-23 18:19:11 +0530

    testing

---
+++ ui/dtest.php
@@ -0,0 +1,33 @@
+
+
+<html>
+	<head>
+		<title>twitter signup</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link href="assets/lib/jquery.bxslider.css" rel="stylesheet" />
+		
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/jquery.bxslider.min.js"></script>
+		<script src="assets/js/download.js?v3.1"></script>
+		<style>
+			.slider-wrap {max-width:1080px; margin:0 auto; padding-top:50px}
+			.bxslider {margin-top:0}
+			.bxslider li {left:0}
+		</style>
+	</head>
+	<body style="background-color: black; color:#fff;">
+		<center>
+			<div class="container" style="padding-top:1%; padding-bottom: 8%;">
+				<div class="row">
+					<div class="col-lg-1"></div>
+					<div class="col-lg-11"><h1>Welcome To See Tweets and Followers of Your Account</h1></div>
+
+				</div>
+				
+			
+			</div>
+		</center>
+	</body>
+</html>
